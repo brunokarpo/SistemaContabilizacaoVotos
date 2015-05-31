0c6386d31d1c41710258e8e8eaf4b1d29ac1eb12
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2015-05-30 22:49:38 -0300

    [Issue #1] Criando um builder para candidato
    Para facilitar a geração de artefatos para teste;
    
    Signed-off-by: Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
    
    No ramo Issue1-Refatorando
    Mudanças a serem submetidas:
    	new file:   SourceCode/ContabilizacaoVotos-parent/votos-entidades/src/main/java/br/ufg/si/poo/entidades/builderUtils/CandidatoBuilder.java

---
+++ SourceCode/ContabilizacaoVotos-parent/votos-entidades/src/main/java/br/ufg/si/poo/entidades/builderUtils/CandidatoBuilder.java
@@ -0,0 +1,72 @@
+package br.ufg.si.poo.entidades.builderUtils;
+
+import br.ufg.si.poo.entidades.Candidato;
+/**
+ * @author Bruno Nogueira<br>
+ *
+ * <p>Classe utilitaria para construir candidatos utilizando o pattern Builder
+ *
+ * @version 1.0.0
+ */
+public class CandidatoBuilder {
+
+	private Candidato instancia;
+
+	/**
+	 * Coloca o nome na instancia de candidato a ser construido
+	 * @param nome do candidato
+	 * @return ConstrutorBuilder
+	 */
+	public CandidatoBuilder comNome(String nome) {
+		instancia.setNome(nome);
+		return this;
+	}
+
+	/**
+	 * Coloca o cargo politico na instancia do candidato a ser construido
+	 * @param cargoPolitico
+	 * @return ConstrutorBuilder
+	 */
+	public CandidatoBuilder comCargoPolitico(String cargoPolitico) {
+		instancia.setCargoPolitico(cargoPolitico);
+		return this;
+	}
+
+	/**
+	 * Coloca o numero na instancia do candidato a ser construido
+	 * @param numero em formato String
+	 * @return ConstrutorBuilder
+	 */
+	public CandidatoBuilder comNumero(String numero) {
+		instancia.setNumero(numero);
+		return this;
+	}
+
+	/**
+	 * Coloca o numero na instancia do candidato a ser construido
+	 * @param numero no formato integer
+	 * @return CandidatoBuilder
+	 */
+	public CandidatoBuilder comNumero(int numero) {
+		return this.comNumero( String.valueOf(numero) );
+	}
+
+	/**
+	 * Coloca a sigla do partido na instancia de candidato a ser construido
+	 * @param siglaPartido
+	 * @return CandidatoBuilder
+	 */
+	public CandidatoBuilder comSiglaPartido(String siglaPartido) {
+		instancia.setSiglaPartido(siglaPartido);
+		return this;
+	}
+
+	/**
+	 * Devolve a instancia do candidato construida por esse Builder
+	 * @return Candidato
+	 */
+	public Candidato construir() {
+		return this.instancia;
+	}
+
+}
